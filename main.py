b182d36b1dd7fa6f931f61276ae2980f89c34dff
dagbolade <37887398+dagbolade@users.noreply.github.com>
2023-01-23 17:08:20 +0000

    added codes for the web app

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-import freebible
+import streamlit as st
+st.title("Bible Chatbot")
+query = st.text_input("Enter your question:")
+if query:
+    # code to handle the query and generate a response goes here
+    response = "This is the chatbot's response to your query."
+    st.write(response)
+
+if query:
+    response = chatbot_model.predict(query)
+    st.write(response)
