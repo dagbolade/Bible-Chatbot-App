22913d4cccf9b44d4db14cc3ec7ac18601f5331d
David Damilola <37887398+dagbolade@users.noreply.github.com>
2023-10-09 12:21:02 +0100

    starting my bible chatbot project

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
+# Importing necessary libraries
 import streamlit as st
-st.title("Bible Chatbot")
-query = st.text_input("Enter your question:")
-if query:
-    # code to handle the query and generate a response goes here
-    response = "This is the chatbot's response to your query."
-    st.write(response)
+from py2neo import Graph
 
-if query:
-    response = chatbot_model.predict(query)
-    st.write(response)
+# Connecting to the knowledge graph
+graph = Graph(host="localhost", user="neo4j", password="neo4j")
+
+# Creating a Streamlit app
+st.title("Bible Chatbot")
+
+# Load the trained model
+model = transformers.BertModel.from_pretrained("path/to/save/model")
+
+# Creating a function for the chatbot
+def chatbot():
+    # Get the user's input
+    user_input = st.text_input("What's on your mind today?")
+
+    # Use the trained model to predict the label
+    label = model.predict(user_input)
+
+    # Querying the knowledge graph
+    result = graph.run(f"MATCH (n:Verse) WHERE n.text CONTAINS '{label}' RETURN n.text, n.book, n.chapter, n.verse")
+
+    # Retrieving the relevant Bible verse from the knowledge graph
+    for record in result:
+        verse = record["n.text"]
+        book = record["n.book"]
+        chapter = record["n.chapter"]
+        verse_num = record["n.verse"]
+
+    # Display the verse to the user
+    st.write("Here's a verse that might help:")
+    st.write(verse)
+    st.write(f"{book} {chapter}:{verse_num}")
+
+chatbot()
